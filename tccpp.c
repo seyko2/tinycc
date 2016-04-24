f2f01c4c25be72de7266973ea80489a529f81ab3
seyko <seyko2@gmail.com>
2016-04-24 18:28:08 +0300

    disable hex float format in asm

--- tccpp.c
+++ tccpp.c
@@ -2906,6 +2906,7 @@ maybe_newline:
             if (!((isidnum_table[c - CH_EOF] & (IS_ID|IS_NUM))
                   || c == '.'
                   || ((c == '+' || c == '-')
+                      && !(parse_flags & PARSE_FLAG_ASM_FILE)
                       && (t == 'e' || t == 'E' || t == 'p' || t == 'P')
                       )))
                 break;
